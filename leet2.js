2ea32619ebe002191dd29d013b948d54fafbf91f
CHRISTO25 <christovarghese2725@gmail.com>
2024-02-28 15:58:24 +0530

    weathetr completeed

--- leet2.js
+++ leet2.js
@@ -1,6 +1,8 @@
 
 var addTwoNumbers = function (l1, l2) {
-    let sum 
+    let sum , one ,two,lf,ls
+    lf=l1
+    ls=l2
     if (l1.length && l2.length == 1) {
         if (l1[0] == 0 && l2[0] == 0) {
             return 0;
@@ -8,12 +10,20 @@ var addTwoNumbers = function (l1, l2) {
         else{
             sum = l1[0]+l2[0]
             sum =Array.from(String(sum),Number)
-            console.log(sum);
+            sum =[...sum].reverse();
+            return sum
         }
     }
-
-
+    one=[...lf].reverse();
+    two=[...ls].reverse();
+     one =parseInt(one.join(''), 10)
+     two =parseInt(two.join(''), 10)
+     sum = one + two
+     console.log(one);
+     sum =Array.from(String(sum),Number)
+     sum =[...sum].reverse()
+     return sum
 }
 
 
-console.log(addTwoNumbers([11], [10]));
+console.log(addTwoNumbers([9,9,9,9,9,9,9],[9,9,9,9]));
